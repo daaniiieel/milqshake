c4ea6add7cbd15e3c59b055a7c927a0da6cbd7ed
ysfchn <54748183+ysfchn@users.noreply.github.com>
2019-12-01 15:31:17 +0300

    Automatic column for dynamic contents

--- js/create_card.js
+++ js/create_card.js
@@ -1,6 +1,6 @@
 function CreateCard(title, content, user, imageSrc) {
 
-	var cardstart = '<div class="mdl-card mdl-shadow--2dp">'
+	var cardstart = '<div class="mdl-card mdl-shadow--2dp" style="margin: 10px;">'
 	+ '<div class="mdl-card__title mdl-card--expand">'
 	+ '<h2 class="mdl-card__title-text">' + title + '</h2></div>';
 
