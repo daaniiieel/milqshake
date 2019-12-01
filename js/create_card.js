533b9ce4e10da8f93cee097aa04ede5d09446171
ysfchn <54748183+ysfchn@users.noreply.github.com>
2019-12-01 15:24:58 +0300

    Added dynamic card create function

---
+++ js/create_card.js
@@ -0,0 +1,19 @@
+function CreateCard(title, content, user, imageSrc) {
+
+	var cardstart = '<div class="mdl-card mdl-shadow--2dp">'
+	+ '<div class="mdl-card__title mdl-card--expand">'
+	+ '<h2 class="mdl-card__title-text">' + title + '</h2></div>';
+
+	var cardmiddle = '<div class="mdl-card__supporting-text">' + content;
+
+	var chip = '<br><br><span class="mdl-chip mdl-chip--contact">'
+    + '<img class="mdl-chip__contact" src="' + imageSrc + '"></img>'
+    + '<span class="mdl-chip__text">' + user + '</span>'
+    + '</span>';
+
+	var cardend = '<div class="mdl-card__actions mdl-card--border">'
+	+ '<a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">View Updates</a>' +
+      '</div></div>';
+
+	$(".page-content").append(cardstart + cardmiddle + chip + '</div>' + cardend); 
+}
