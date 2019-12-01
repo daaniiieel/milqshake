79cd45f6d6499cb542c37ea4bf837abf8fd62900
ysfchn <54748183+ysfchn@users.noreply.github.com>
2019-12-01 15:45:49 +0300

    Updated card design

--- js/create_card.js
+++ js/create_card.js
@@ -1,19 +1,19 @@
 function CreateCard(title, content, user, imageSrc) {
 
-	var cardstart = '<div class="mdl-card mdl-shadow--2dp" style="margin: 10px;">'
-	+ '<div class="mdl-card__title mdl-card--expand">'
+	var cardstart = '<div class="mdl-cell"><div class="mdl-card mdl-shadow--2dp" style="width: 100%;">'
+	+ '<div class="mdl-card__title">'
 	+ '<h2 class="mdl-card__title-text">' + title + '</h2></div>';
 
-	var cardmiddle = '<div class="mdl-card__supporting-text">' + content;
+	var cardmiddle = '<div class="mdl-card__supporting-text mdl-card--expand">' + content;
 
-	var chip = '<br><br><span class="mdl-chip mdl-chip--contact">'
+	var chip = '<span class="mdl-chip mdl-chip--contact" style="background-color: transparent; width: 100%;">'
     + '<img class="mdl-chip__contact" src="' + imageSrc + '"></img>'
     + '<span class="mdl-chip__text">' + user + '</span>'
     + '</span>';
 
 	var cardend = '<div class="mdl-card__actions mdl-card--border">'
-	+ '<a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">View Updates</a>' +
-      '</div></div>';
+	+ chip +
+      '</div></div></div>';
 
-	$(".page-content").append(cardstart + cardmiddle + chip + '</div>' + cardend); 
+	$(".page-content").append(cardstart + cardmiddle + '</div>' + cardend); 
 }
